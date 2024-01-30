c5cbcca44c1b8c3b8833e2b1d2acf7b668b53254
mbright90 <michael.bright90@icloud.com>
2024-01-30 16:10:02 +0000

    Add express-compile-sass config

--- server.js
+++ server.js
@@ -2,16 +2,18 @@ const express = require("express");
 const mongoose = require("mongoose");
 const path = require("path");
 
+const compileSass = require("express-compile-sass");
+
 const messagesRouter = require("./routes/messageRoutes");
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 app.set("view engine", "ejs");
 
 // ----- DB connection ----- //
 mongoose.set("strictQuery", false);
-const mongoDB = `mongodb+srv://${process.env.DbUSER}:${process.env.DbPW}@messageboard.9sxrucw.mongodb.net/`;
+const mongoDB = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PW}@messageboard.9sxrucw.mongodb.net/`;
 
 async function main() {
   mongoose.connect(mongoDB);
@@ -21,7 +23,16 @@ main().catch((err) => console.log(err));
 
 // ----- Middleware ----- //
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(
+  compileSass({
+    root: path.join(__dirname, "public"),
+    sourceMap: true,
+    sourceComments: true,
+    watchFiles: true,
+    logToConsole: false,
+  }),
+);
+app.use(express.static("public"));
 
 // ----- Routes ----- //
 
@@ -34,7 +45,7 @@ app.use("/messages", messagesRouter);
 // ----- Error Handling ----- //
 
 app.use((req, res) => {
-  res.statusCode(404).render("404", { title: "404" });
+  res.status(404).render("404", { title: "404" });
 });
 
 app.listen(PORT, () => console.log(`App running on ${PORT}`));
