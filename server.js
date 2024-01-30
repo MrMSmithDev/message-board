599ab42add04b8f8d5ea16edfe05b03b1479b84c
mbright90 <michael.bright90@icloud.com>
2024-01-30 14:10:06 +0000

    Add env mongoDb login

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 const express = require("express");
 const mongoose = require("mongoose");
-
 const path = require("path");
+
+const messagesRouter = require("./routes/messageRoutes");
+
+const PORT = 3000;
 
 const app = express();
 app.set("view engine", "ejs");
 
 // ----- DB connection ----- //
 mongoose.set("strictQuery", false);
-const mongoDB = `mongodb+srv://admin:${process.env.DbPW}@messageboard.9sxrucw.mongodb.net/`;
+const mongoDB = `mongodb+srv://${process.env.DbUSER}:${process.env.DbPW}@messageboard.9sxrucw.mongodb.net/`;
 
 async function main() {
   mongoose.connect(mongoDB);
@@ -26,10 +29,12 @@ app.get("/", (req, res) => {
   res.render("index", { title: "Home" });
 });
 
+app.use("/messages", messagesRouter);
+
 // ----- Error Handling ----- //
 
 app.use((req, res) => {
   res.statusCode(404).render("404", { title: "404" });
 });
 
-app.listen(3000);
+app.listen(PORT, () => console.log(`App running on ${PORT}`));
