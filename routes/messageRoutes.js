e7653d517efa5772014d3d9c55d24dff2420f68a
mbright90 <michael.bright90@icloud.com>
2024-01-30 14:03:27 +0000

    Add message routes

---
+++ routes/messageRoutes.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const controller = require("../controllers/messageController");
+
+const router = express.Router();
+
+router.get("/", controller.messages_index);
+
+router.post("/", controller.message_post);
+
+router.get("/new", controller.message_new_get);
+
+router.get("/:id", controller.message_details_get);
+
+router.delete("/:id", controller.message_delete);
+
+module.exports = router;
