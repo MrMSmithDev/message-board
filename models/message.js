d24a47ac37265f8970f44fc1acb9c2c2933fb675
mbright90 <michael.bright90@icloud.com>
2024-02-06 13:56:45 +0000

    Convert pre schema function to default within initiation

--- models/message.js
+++ models/message.js
@@ -3,14 +3,7 @@ const mongoose = require("mongoose");
 const MessageSchema = new mongoose.Schema({
   message: { type: String, require: true, maxLength: 250 },
   author: { type: String, required: true, maxLength: 50 },
-  date_posted: { type: Date, required: true },
-});
-
-MessageSchema.pre("save", (next) => {
-  if (!this.date_posted) {
-    this.date_posted = new Date();
-  }
-  next();
+  date_posted: { type: Date, default: new Date() },
 });
 
 module.exports = mongoose.model("Message", MessageSchema);
