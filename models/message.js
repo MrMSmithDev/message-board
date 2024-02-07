e7acf6dd5147bb4a6f369107a00c1f6d7d767a97
mbright90 <michael.bright90@icloud.com>
2024-02-07 10:41:41 +0000

    Remove unnecessary pre Schema function

--- models/message.js
+++ models/message.js
@@ -3,10 +3,8 @@ const mongoose = require("mongoose");
 const MessageSchema = new mongoose.Schema({
   message: { type: String, require: true, maxLength: 250 },
   author: { type: String, required: true, maxLength: 50 },
-  avatar_svg: { type: SVGElement, required: true },
+  avatar_svg: { type: String, required: true },
   date_posted: { type: Date, default: new Date() },
 });
-
-// TODO: add pre to add avatar on save
 
 module.exports = mongoose.model("Message", MessageSchema);
