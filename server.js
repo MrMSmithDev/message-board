28aa9bbb5d05c1338a2f4b36159afa92b36cc37c
mbright90 <michael.bright90@icloud.com>
2024-02-17 14:58:11 +0000

    Add middleware for production

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const mongoose = require("mongoose");
 
+const compression = require("compression");
+const helmet = require("helmet");
+const rateLimit = require("express-rate-limit");
+
 const messagesRouter = require("./routes/messageRoutes");
 
+const isProd = process.env.NODE_ENV === "production" || "development";
 const PORT = process.env.PORT || 3000;
 
 const app = express();
@@ -21,6 +26,20 @@ app.use(express.static("public"));
 
 app.use(express.urlencoded({ extended: true })); // parse form content
 app.use(express.json());
+
+if (isProd === "production") {
+  app.use(compression());
+  app.use(helmet());
+
+  const limiter = rateLimit({
+    windowMs: 1 * 60 * 1000,
+    max: 20,
+  });
+
+  app.use(limiter);
+} else {
+  console.log("Not Prod");
+}
 
 // ----- Routes ----- //
 
